72a81f782c2ace2cdeadf737da22a1313431d977
thefosk <marco@mashape.com>
2017-04-19 14:37:32 -0700

    feat(cli) disable automatic migrations

--- kong/cmd/start.lua
+++ kong/cmd/start.lua
@@ -17,7 +17,10 @@ local function execute(args)
   local dao = assert(DAOFactory.new(conf))
   xpcall(function()
     assert(prefix_handler.prepare_prefix(conf, args.nginx_conf))
-    assert(dao:run_migrations())
+    if args.run_migrations then
+      assert(dao:run_migrations())
+    end
+    assert(dao:are_migrations_uptodate())
     assert(nginx_signals.start(conf))
     log("Kong started")
   end, function(e)
@@ -39,9 +42,10 @@ Start Kong (Nginx and other configured services) in the configured
 prefix directory.
 
 Options:
- -c,--conf    (optional string) configuration file
- -p,--prefix  (optional string) override prefix directory
- --nginx-conf (optional string) custom Nginx configuration template
+ -c,--conf        (optional string)   configuration file
+ -p,--prefix      (optional string)   override prefix directory
+ --nginx-conf     (optional string)   custom Nginx configuration template
+ --run-migrations (optional boolean)  optionally run migrations on the DB 
 ]]
 
 return {
